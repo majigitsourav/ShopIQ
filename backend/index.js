2993b15a3faa2f5da774dba80cbb98d9fe0057d1
Sourav Maji <91647481+majigitsourav@users.noreply.github.com>
2026-02-22 00:12:42 +0530

    Update CORS origin to include admin frontend

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["https://shopiq-frontend.onrender.com","http://localhost:5174"],
+    origin:["https://shopiq-frontend.onrender.com","https://shopiq-admin.onrender.com"],
     credentials:true
 }));
 
