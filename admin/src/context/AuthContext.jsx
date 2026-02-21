0b31a287b2d23c3571f549417a25b79cf351c8e9
Sourav Maji <91647481+majigitsourav@users.noreply.github.com>
2026-02-22 00:44:31 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
 export const authDataContext = createContext();
 const AuthContext = ({children}) => {
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://shopiq-backend.onrender.com";
   let value = {
     serverUrl
   }
