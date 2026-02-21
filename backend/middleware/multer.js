0eafb5645f5f0dd79b5458d808d4ba8269d997d9
Sourav Maji <91647481+majigitsourav@users.noreply.github.com>
2026-02-22 02:03:26 +0530

    Change multer storage to memory storage
    Switched multer storage from disk to memory for production.

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,13 +1,17 @@
 import multer from 'multer';
 //This is the configuration for multer to store the uploaded files in the public folder with the original name of the file ,,cloudinary will be used to store the files in the cloud and the url of the file will be stored in the database
-let storage = multer.diskStorage({
-    destination:(req,res,cb)=>{
-        cb(null,'./public')
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
-});
+// let storage = multer.diskStorage({
+//     destination:(req,res,cb)=>{
+//         cb(null,'./public')
+//     },
+//     filename:(req,file,cb)=>{
+//         cb(null,file.originalname)
+//     }
+// });
+
+// For production
+const storage = multer.memoryStorage();
+
 
 //This is the middleware that will be used to upload the file to the server
 let upload = multer({storage:storage});
