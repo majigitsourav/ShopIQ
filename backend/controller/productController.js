62c17baf7fbdb15ab8ba8c0f33e2b1f46c778336
Sourav Maji <91647481+majigitsourav@users.noreply.github.com>
2026-02-22 02:11:30 +0530

    Update image upload handling in addProduct
    Change image upload method to use buffer instead of path.

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -3,11 +3,14 @@ import Product from "../model/productModel.js";
 
 export const addProduct = async(req,res)=>{
     try {
+        if (!req.files || !req.files.image1) {
+            return res.status(400).json({ message: "Images not received" });
+        }
         let {name,description,price,category,subCategory,sizes,bestseller} = req.body;
-        let image1 = await uploadOnCloudinary(req.files.image1[0].path);
-        let image2 = await uploadOnCloudinary(req.files.image2[0].path);
-        let image3 = await uploadOnCloudinary(req.files.image3[0].path);
-        let image4 = await uploadOnCloudinary(req.files.image4[0].path);
+        let image1 = await uploadOnCloudinary(req.files.image1[0].buffer);
+        let image2 = await uploadOnCloudinary(req.files.image2[0].buffer);
+        let image3 = await uploadOnCloudinary(req.files.image3[0].buffer);
+        let image4 = await uploadOnCloudinary(req.files.image4[0].buffer);
 
         let productData = {
             name,
